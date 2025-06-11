c04ad53602d45f1f9a3caf45be61e49a7ba46416
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2025-06-11 18:05:14 +0100

    LPE-606: Add NoResults page

---
+++ test/controllers/NoResultsControllerSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import models._
+import pages._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.NoResultsView
+
+import java.time.format.DateTimeFormatter
+import java.time.{ZoneId, ZonedDateTime}
+import java.util.Locale
+
+class NoResultsControllerSpec extends SpecBase {
+
+  "No Results Controller" - {
+    "must return OK and the correct view for a GET" in {
+      val userAnswers = emptyUserAnswers
+        .set(page = WhatIsTheMembersNamePage, value = MemberDetails("Pearl", "Harvey")).success.value
+        .set(page = MembersDobPage, value = MembersDob(1, 1, 2022)).success.value
+        .set(page = MembersNinoPage, value = MembersNino("AB123456A")).success.value
+        .set(page = MembersPsaCheckRefPage, value = MembersPsaCheckRef("PSA12345678A")).success.value
+
+      val application = applicationBuilder(userAnswers = userAnswers).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.NoResultsController.onPageLoad().url)
+        val result = route(application, request).value
+        val view = application.injector.instanceOf[NoResultsView]
+
+        val memberDetails: MemberDetails = MemberDetails("Pearl", "Harvey")
+        val membersDob: MembersDob = MembersDob(1, 1, 2022)
+        val membersNino: MembersNino = MembersNino("AB123456A")
+        val membersPsaCheckRef: MembersPsaCheckRef = MembersPsaCheckRef("PSA12345678A")
+
+        val dateTimeWithZone = ZonedDateTime.now(ZoneId.of("Europe/London"))
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy 'at' h:mma")
+        val localDateTime = dateTimeWithZone.format(formatter.withLocale(Locale.UK))
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          memberDetails,
+          membersDob,
+          membersNino,
+          membersPsaCheckRef,
+          localDateTime,
+        )(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+      val application = applicationBuilder(userAnswers = emptyUserAnswers).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ResultsController.onPageLoad().url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+
+}
