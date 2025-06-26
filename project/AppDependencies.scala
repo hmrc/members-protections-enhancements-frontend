1c0e8b50e8054f3e66cbab798a871fae8d57840c
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2025-06-26 10:11:00 +0100

    LPE-721: Bump all upgradable dependencies. Enable rebrand

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "11.12.0",
-    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.2.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.6.0",
+    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                              % "2.13.0",
     "com.chuusai"        %% "shapeless"                              % "2.4.0-M1",
