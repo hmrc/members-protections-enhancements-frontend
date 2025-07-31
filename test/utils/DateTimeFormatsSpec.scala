29eb098894fcca244a560ec48a71941427432aed
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2025-07-31 16:56:12 +0100

    LPE-783: Add long and short month format parsers.

--- test/utils/DateTimeFormatsSpec.scala
+++ test/utils/DateTimeFormatsSpec.scala
@@ -16,12 +16,16 @@
 
 package utils
 
+import com.github.tomakehurst.wiremock.common.DateTimeParser
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import play.api.i18n.Lang
-import utils.DateTimeFormats.{dateTimeFormat, getCurrentDateTimestamp}
+import utils.DateTimeFormats.{dateTimeFormat, getCurrentDateTimestamp, longMonthFormat, shortMonthFormat}
 
+import java.time.format.{DateTimeFormatter, DateTimeParseException}
+import java.time.temporal.ChronoField
 import java.time.{LocalDate, ZoneId, ZonedDateTime}
+import scala.util.control.Exception.nonFatalCatch
 
 class DateTimeFormatsSpec extends AnyFreeSpec with Matchers {
 
@@ -51,6 +55,59 @@ class DateTimeFormatsSpec extends AnyFreeSpec with Matchers {
         ZonedDateTime.of(2025, 11, 11, 11, 11, 11, 11, ZoneId.of("Europe/London"))
       )
       result mustEqual "11 November 2025 at 11:11am"
+    }
+  }
+
+  def testForMonthString(monthStr: String, monthInt: Int, parser: DateTimeFormatter): Unit =
+    s"should convert the string: $monthStr to the int: $monthInt" in {
+      nonFatalCatch.opt(parser.parse(monthStr)).map(
+        _.get(ChronoField.MONTH_OF_YEAR)
+      ) mustBe Some(monthInt)
+    }
+
+  ".shortMonthFormat" - {
+    val validValues: Seq[(String, Int)] = Seq(
+      "Jan" -> 1,
+      "Feb" -> 2,
+      "Mar" -> 3,
+      "Apr" -> 4,
+      "May" -> 5,
+      "Jun" -> 6,
+      "Jul" -> 7,
+      "Aug" -> 8,
+      "Sept" -> 9,
+      "Oct" -> 10,
+      "Nov" -> 11,
+      "Dec" -> 12
+    )
+
+    validValues.foreach(scenario => testForMonthString(scenario._1, scenario._2, shortMonthFormat))
+
+    "should not parse any incorrect string" in {
+      assertThrows[DateTimeParseException](shortMonthFormat.parse("incorrect"))
+    }
+  }
+
+  ".longMonthFormat" - {
+    val validValues: Seq[(String, Int)] = Seq(
+      "January" -> 1,
+      "February" -> 2,
+      "March" -> 3,
+      "April" -> 4,
+      "May" -> 5,
+      "June" -> 6,
+      "July" -> 7,
+      "August" -> 8,
+      "September" -> 9,
+      "October" -> 10,
+      "November" -> 11,
+      "December" -> 12
+    )
+
+    validValues.foreach(scenario => testForMonthString(scenario._1, scenario._2, longMonthFormat))
+
+    "should not parse any incorrect string" in {
+      assertThrows[DateTimeParseException](longMonthFormat.parse("incorrect"))
     }
   }
 }
